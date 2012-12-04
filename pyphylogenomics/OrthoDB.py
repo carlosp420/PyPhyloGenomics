70095010e4082de8faca3d589d94addd6fd69ead
Carlos.Peña <mycalesis@gmail.com>
2012-12-04 23:33:20 +0200

    arg species_name

--- pyphylogenomics/OrthoDB.py
+++ pyphylogenomics/OrthoDB.py
@@ -5,34 +5,17 @@ import sys;
 
 
 
-# -----------------------------------------------------------------------------
-description = '''This module uses the table OrthoDB6_Arthropoda_tabtext.csv
-downloaded from OrthoDB (the database of orthologous groups) to
-extract certain features and objects described below.  '''
-
-parser = argparse.ArgumentParser(description=description);
-parser.add_argument('-i', '--input', action='store', 
-		metavar='OrthoDB_xyz.csv',
-		required=True, dest='in_file',
-		help='OrthoDb table as input file');
-parser.add_argument('-o', '--output', action='store', 
-		metavar='output_table.csv',
-		required=True, dest='out_file',
-		help='parsed data as csv file');
-args = parser.parse_args();
-
-
 
 
 def copies_per_gene(in_file):
-    '''Creates a dictionary with the number of copies per gene and per specie.
+    '''
+	Creates a dictionary with the number of copies per gene and per species.
     
     The dictionary has as keys tuples=(species_name, gene_name) and
-    values=integer that represents the number of copies of that gene
+    values=integer that represent the number of copies of that gene
     in the species.
     '''
 
-    print "running..."
 
     dictio = {}
 
@@ -40,6 +23,7 @@ def copies_per_gene(in_file):
     genes=set()
     species=set()
 
+    in_file = open(in_file, "rb");
     in_file.next() # skip header
     for line in in_file:
         specie = line.strip().split('\t')[4]
@@ -64,16 +48,17 @@ def single_copy_genes(in_file, species_name):
     of the function 'copies_per_gene'.
     '''
 
-    print "running..."
+    print "\n";
+    print "Looking for single-copy genes for species "+ str(species_name) +":";
     
     dictio = copies_per_gene(in_file)
     for key in dictio:
         if species_name in key and dictio[key] == 1:
-            print key[1]
+            print "Found gene: " + str(key[1]);
         else:
             pass
 
-    in_file.close()
+	#in_file.close()
    
 
 def single_copy_in_species(in_file, gene_name):
@@ -101,12 +86,12 @@ def copies_per_gene_table(in_file, out_file):
 	This script stores the number of copies a gene has per species in a file.
 
     The output file is a table, where each row is a gene, and each columm
-    is a specie. The values in the table are the number of gene copies.
+    is a species. The values in the table are the number of gene copies.
     Note: the first row in the otput table is the number of single-copy
     genes in a given species.
     '''
     
-    print "running..."
+    print "Parsing input file..."
 
     dictio = copies_per_gene(in_file)
     
@@ -142,17 +127,52 @@ def copies_per_gene_table(in_file, out_file):
         out_file.write("\t".join(row) + '\n')
 
 
-    in_file.close()
+	#in_file.close()
     out_file.close()
 
     print "\nOUTPUT FILE WAS GENERATED!"        
 
 
-### EXAMPLE:
-# in_file = open(raw_input("Input file path and name: "),'rb')
-# out_file = open(raw_input("Output file path and desired name: "),'w')
-# copies_per_gene_table(in_file,out_file)
+# -----------------------------------------------------------------------------
+def main():
+	description = '''This module uses the table OrthoDB6_Arthropoda_tabtext.csv
+		downloaded from OrthoDB (the database of orthologous groups) to
+		extract certain features and objects described below.  '''
 
-in_file = open(args.in_file, 'rb');
-out_file = open(args.out_file, 'w');
-copies_per_gene_table(in_file, out_file);
+	parser = argparse.ArgumentParser(description=description);
+	parser.add_argument('-i', '--input', action='store', 
+			metavar='OrthoDB_xyz.csv',
+			required=True, dest='in_file',
+			help='OrthoDb table as input file');
+	parser.add_argument('-o', '--output', action='store', 
+			metavar='output_table.csv',
+			required=True, dest='out_file',
+			help='parsed data as csv file');
+	parser.add_argument('-s', '--species', action='store', 
+			metavar='Bombyx_mori',
+			dest='species_name',
+			help='return single-copy genes for given species name (use \
+				underscore to join genus and species name)');
+	
+	args = parser.parse_args();
+	
+	
+	in_file = args.in_file.strip();
+	out_file = open(args.out_file, 'w');
+	species_name = args.species_name;
+
+	## do default processing
+	print "Finding number of copies per gene..."
+	copies_per_gene_table(in_file, out_file);
+
+	## if --species argument given
+	if species_name != None:
+		species_name = species_name.strip();
+
+		# species will be "Bombyx mori"
+		species_name = species_name.replace("_", " ");
+		single_copy_genes(in_file, species_name);
+
+
+if __name__ == "__main__":
+	main();
