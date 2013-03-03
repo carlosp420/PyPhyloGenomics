75edf770dbc64cf3501f2a8280baace70e223628
Carlos.Peña <mycalesis@gmail.com>
2013-03-04 01:59:31 +0200

    setup fixes

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from distutils.core import setup
 
-setup(name='PyPhyloGenomics', version='0.1.0',
+setup(
+		name='PyPhyloGenomics', 
+		version='0.1.0',
+		packages = ['pyphylogenomics'],
 		author="Carlos Pena, Victor Solis, Pavel Matos, Chris Wheat",
 		author_email="mycalesis@gmail.com",
 		maintainer="Carlos Pena, Victor Solis, Pavel Matos, Chris Wheat",
@@ -12,11 +15,15 @@ setup(name='PyPhyloGenomics', version='0.1.0',
 		long_description=open('README.txt').read(),
 		keywords="DNA, genomics, genomes, phylogenetics, genes",
 		url="http://nymphalidae.utu.fi",
+		download_url = "https://github.com/carlosp420/PyPhyloGenomics/",
 		classifiers=[
 					"Programming Language :: Python",
 					("Topic :: Scientific/Engineering :: Bio-Informatics"),
 					("Intended Audience :: Science/Research"),
+					("License :: GPL"),
+					("Operative System :: OS Independent"),
 					("Environment :: Console"),
 					],
 
-		install_requires=['biopython','MySQLdb']);
+		requires=['biopython','MySQLdb']
+		);
