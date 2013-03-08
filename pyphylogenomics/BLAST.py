75c730f5c5bc6be989c81679263873ee2e1bc0bf
Carlos Pena <mycalesis@gmail.com>
2013-03-08 18:06:18 +0200

    some work on BLAST

--- pyphylogenomics/BLAST.py
+++ pyphylogenomics/BLAST.py
@@ -18,13 +18,13 @@ from Bio import SeqIO;
 
 def get_cds(genes, cds_file):
 	'''
-	Writes a FASTA file containing CDS sequences: ``pulled_seqs.fas``
+	Writes a FASTA file containing CDS sequences: ``pulled_seqs.fa``
 	
 	``genes`` argument is a list of gene IDs.
 	``cds_file`` is the file name of predicted CDS for a species in FASTA format.
 
 	'''
-	outfile = open("pulled_seqs.fas", "w");
+	outfile = open("pulled_seqs.fa", "w");
 
 	i = 0;
 	for seq_record in SeqIO.parse(cds_file, "fasta"):
@@ -35,18 +35,37 @@ def get_cds(genes, cds_file):
 			i = i + 1;
 	outfile.close();
 
-	print i, " sequences were written to file pulled_seqs.fas";
+	print i, " sequences were written to file pulled_seqs.fa";
 
 
 def makeblastdb(FASTA_file):
 	command  = 'makeblastdb -in ' + FASTA_file;
 	command += ' -dbtype nucl -parse_seqids -input_type fasta'; 
-	p = subprocess.Popen(command, shell=True) 
+	p = subprocess.check_output(command, shell=True) 
+	print p
 	
 	command  = 'blastdb_aliastool -dblist "' + FASTA_file + '" ';
-	command += '-dbtype nucl -out ' +  FASTA_file + ' -title "db"';
-	p = subprocess.Popen(command, shell=True)
+	command += '-dbtype nucl -out ' +  FASTA_file + ' -title "' + FASTA_file + '"';
+	p = subprocess.check_output(command, shell=True)
+	print p
 
 	command  = 'makembindex -input ' + FASTA_file;
-	command += ' -iformat fasta -output db'
-	p = subprocess.Popen(command, shell=True)
+	command += ' -iformat fasta -output ' + FASTA_file;
+	p = subprocess.check_output(command, shell=True)
+	print p
+
+
+def blastn(seqs, genome):
+	'''
+	Performs a BLASTn of user's sequences against a genome. It will create a
+	BLAST database from the genome file first.
+
+	``seqs`` argument is a FASTA file of users sequences.
+	``genome`` argument is a FASTA file of a species genome.
+	'''
+
+	# do a BLAST database first
+	makeblastdb(genome);
+
+
+
