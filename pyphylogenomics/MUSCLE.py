505c31b9b46db98b50644ac475138b84fb1545ce
Carlos.Peña <mycalesis@gmail.com>
2013-04-17 21:31:49 +0300

    saving alignments into folder

--- pyphylogenomics/MUSCLE.py
+++ pyphylogenomics/MUSCLE.py
@@ -4,63 +4,72 @@ from Bio.Align.Applications import MuscleCommandline
 import subprocess
 import sys
 import time
+import os
 
-def batchAlignment(files, out_path):
-	'''
-	Reads in sequences from files, group homologous sequences and aligns them.
+def batchAlignment(files):
+    '''
+    Reads in sequences from files, group homologous sequences and aligns them.
 
-	files = a list of file names. The first file in the list should
-	be the master file (e.g. Bombyx mori).
-	out_path = the folder where the alignment files shall be store.
-	'''
-	
-	for idx, f in enumerate(files): # Read each file into a dictionary.
-		exec("handle%d = %s" % (idx+1, "SeqIO.index(f,'fasta')")) 
+    ``files`` a list of file names. The first file in the list should
+    be the master file (e.g. exon sequences of *Bombyx mori*).
 
-	count = 0 # To count the number of homologous groups.
-	print "DO NOT FREAK OUT!"
-	print "Command line shells will pop up now."
-	time.sleep(3)
+    Example:
+    
+    >>> from PyPhyloGenomics import MUSCLE
+    >>> files = ['Bmori_exons.fas', 'Danaus_exons.fas','Heliconius_exons.fas','Manduca_exons.fas']
+    >>> MUSCLE.batchAlignment(files)
 
-	print "Parsing the master file"
-	for ID1 in handle1: # Parsing the master file.
-		seqs_list = []
-		seqs_list.append(SeqRecord(handle1[ID1].seq, id = ID1))
-		print "Pooling gene %s" % ID1;
-
-		idx = len(files)
-		while idx > 1: # Parsing the other files.
-			script = '''for ID2 in handle%d:
-	if ID1 == ID2.split('-')[0]:
-		seqs_list.append(SeqRecord(handle%d[ID2].seq, id = ID2))'''
-			exec(script % (idx, idx))
-			idx -= 1
-
-		#TODO: Add a command for PSAs, for cases where the homologous group
-													# has only 2 sequences.
-
-		if len(seqs_list) > 2: # Now perfom the MSA.
-			count += 1
-
-			muscle_cline = MuscleCommandline(out=out_path+"\\"+ID1+
-											 ".aln", fasta=True)
-		
-			child = subprocess.Popen(str(muscle_cline),
-									 stdin = subprocess.PIPE,
-									 stderr = subprocess.PIPE,
-									 shell = (sys.platform!="win32")) # In case the user's machine is not Windows.
-
-			SeqIO.write(seqs_list, child.stdin, "fasta")
-			child.stdin.close()
-
-			time.sleep(3) # Wait 3 seconds to not fill off the screen with CMDs.
-
-	print "%d alignments have been saved in %s." % (count, out_path)
+    All aligned sequences will be written into a folder called ``alignments`` as
+    FASTA files (one file per exon).
+    '''
+    
+    # create folder for saving alignments
+    folder = "alignments";
+    if not os.path.exists(folder):
+        os.makedirs(folder);
 
 
-#### USE ####
+    for idx, f in enumerate(files): # Read each file into a dictionary.
+        exec("handle%d = %s" % (idx+1, "SeqIO.index(f,'fasta')")) 
 
-# The first file in the list below should be Bombyx mori sequences:
-# files = ['Bmori_exons.fas', 'Danaus_exons.fas','Heliconius_exons.fas','Manduca_exons.fas']
-# out_path = C:\Users\...\NewFolder  # Debes crear un nuevo folder para q se guarden los alineamientos.
-# batchAlignment(files, out_path)
+    count = 0 # To count the number of homologous groups.
+    print "If you are using Windows..."
+    time.sleep(1);
+    print "many command line shells might pop up now."
+    time.sleep(3)
+
+    for ID1 in handle1: # Parsing the master file.
+        seqs_list = []
+        seqs_list.append(SeqRecord(handle1[ID1].seq, id = ID1))
+        print "Pooling gene %s" % ID1;
+
+        idx = len(files)
+        while idx > 1: # Parsing the other files.
+            script = '''for ID2 in handle%d:
+    if ID1 == ID2.split('-')[0]:
+        seqs_list.append(SeqRecord(handle%d[ID2].seq, id = ID2))'''
+            exec(script % (idx, idx))
+            idx -= 1
+
+        #TODO: Add a command for PSAs, for cases where the homologous group
+                                                    # has only 2 sequences.
+
+        if len(seqs_list) > 2: # Now perfom the MSA.
+            count += 1
+
+            path = os.path.join(folder, ID1 + ".fas");
+            muscle_cline = MuscleCommandline(out=path, fasta=True)
+        
+            child = subprocess.Popen(str(muscle_cline),
+                                     stdin = subprocess.PIPE,
+                                     stderr = subprocess.PIPE,
+                                     shell = (sys.platform!="win32")) # In case the user's machine is not Windows.
+
+            SeqIO.write(seqs_list, child.stdin, "fasta")
+            child.stdin.close()
+
+            time.sleep(3) # Wait 3 seconds to not fill off the screen with CMDs.
+
+    print "%d alignments have been saved in the folder \"%s\"" % (count, folder)
+
+
