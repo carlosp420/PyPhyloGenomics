5f792d2e4aed89e6b612a0515d3f7f6543e2af70
Carlos Peña <mycalesis@gmail.com>
2013-08-31 00:51:58 +0300

    OrthoDB unittest end

--- Tests/test_OrthoDB.py
+++ Tests/test_OrthoDB.py
@@ -2,13 +2,19 @@ import unittest
 from pyphylogenomics import OrthoDB
 
 
-class OrthoDBTest_single_copy_genes(unittest.TestCase):
+class OrthoDBTest(unittest.TestCase):
 
     def test_single_copy_genes(self):
         """We are using only part of the original OrthoDB6 file"""
         result = OrthoDB.single_copy_genes("OrthoDB/OrthoDB6_Arthropoda_tabtext.csv", \
                     "Bombyx mori")
         self.assertEqual(len(result), 444)
+
+    def test_single_copy_in_species(self):
+        result = \
+        OrthoDB.single_copy_in_species("OrthoDB/OrthoDB6_Arthropoda_tabtext.csv", \
+                    "Znev_18776")
+        self.assertEqual(result[0], "Zootermopsis nevadensis")
 
 
 if __name__ == "__main__":
