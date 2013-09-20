b91345600e93c7b23cd12793f5809c6971aa413a
Carlos Peña <mycalesis@gmail.com>
2013-09-20 20:27:09 +0300

    fixing run_test and removing it from readme

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-[![Build Status](https://travis-ci.org/carlosp420/PyPhyloGenomics.png?branch=master)](https://travis-ci.org/carlosp420/PyPhyloGenomics)
-
 #PyPhyloGenomics
 A package to work on Phylogenomics.
 
--- Tests/run_tests.py
+++ Tests/run_tests.py
@@ -1,6 +1,9 @@
 import os
 
 def main():
+    dir = os.getcwd()
+    if dir[-5:] != "Tests":
+        os.chdir("Tests")
     names = os.listdir(os.curdir)
     for name in names:
         if name[:5] == "test_" and name[-3:] == ".py":
