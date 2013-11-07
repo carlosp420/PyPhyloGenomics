5d7e5e84d907dcca87c788913cbbf073b7b26c83
carlosp420 <mycalesis@gmail.com>
2013-11-07 13:13:47 +0200

    Adding assembly_velvet.sh files
    These are used when doing assembly of contigs. If they are not in current
    folder they get downloaded from github.

---
+++ assembly_velvet.sh
@@ -0,0 +1,5 @@
+for k in 31 29 27 25 23 21 19 17 15 13 11 9 7 5 3 1
+do
+   velveth_de test $k -fastq -shortPaired $1
+   velvetg_de test  -min_contig_lgth 100
+done
---
+++ assembly_velvet2.sh
@@ -0,0 +1,2 @@
+velveth_de test $2 -fastq -shortPaired $1
+velvetg_de test -cov_cutoff 30.0 
