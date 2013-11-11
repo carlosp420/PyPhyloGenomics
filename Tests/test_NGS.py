056f6ef8ef17e18b3d7567b09b36f285653f493a
carlosp420 <mycalesis@gmail.com>
2013-11-11 15:26:18 +0200

    Added UnitTest for filter_reads function
    It is an internal function in the module NGS that extracts the FASTQ reads
    according to a BLAST table output and stores them into gene bins.

---
+++ Tests/test_NGS.py
@@ -0,0 +1,35 @@
+import unittest
+import os
+import os.path
+from pyphylogenomics import NGS
+import subprocess
+import glob
+
+
+class NGSTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_filter_reads(self):
+        folder = "NGS"
+
+        for i in glob.glob(os.path.join("NGS", "gene*")):
+            os.remove(i)
+
+        ion_chunk = os.path.join("NGS", "_reaa.fastq")
+        blast_chunk = os.path.join("NGS", "_reaa.csv")
+        NGS.filter_reads(ion_chunk, blast_chunk, folder)
+
+        cmd = "cat " + os.path.join("NGS", "gene*")
+        cmd += " | grep -c '^@'"
+        p = subprocess.check_output(cmd, shell=True)
+
+        for i in glob.glob(os.path.join("NGS", "gene*")):
+            os.remove(i)
+        self.assertEqual(int(p.strip()), 20)
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner(verbosity = 2)
+    unittest.main(testRunner=runner)
+
